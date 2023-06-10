d8ee6ef14492c8edaa3e928f7358cd06fbf27e54
Peter Lehnhardt <e0017@outlook.com>
2023-06-10 10:33:59 +0200

    Add support for auto-completion

--- nvim/init.lua
+++ nvim/init.lua
@@ -119,6 +119,12 @@ require("packer").startup(function(use)
     use "purescript-contrib/purescript-vim"
     use 'wuelnerdotexe/vim-astro'
 
+    -- Language Support
+    use "neovim/nvim-lspconfig"
+    use "purescript-contrib/purescript-vim"
+    use "hrsh7th/nvim-cmp"
+    use "hrsh7th/cmp-nvim-lsp"
+
     -- Others
     use "lambdalisue/suda.vim"
 
@@ -130,10 +136,49 @@ end)
 
 
 -- LSP Setup
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
 local lspconfig = require("lspconfig")
-lspconfig.tsserver.setup {}
-lspconfig.purescriptls.setup {}
-lspconfig.astro.setup {}
+local servers = {"tsserver", "purescriptls", "eslint", "astro"}
+for _, lsp in ipairs(servers) do
+  lspconfig[lsp].setup {
+    -- on_attach = my_custom_on_attach,
+    capabilities = capabilities,
+  }
+end
+
+-- nvim-cmp setup
+local cmp = require("cmp")
+cmp.setup {
+  mapping = cmp.mapping.preset.insert({
+    ['<C-u>'] = cmp.mapping.scroll_docs(-4), -- Up
+    ['<C-d>'] = cmp.mapping.scroll_docs(4), -- Down
+    -- C-b (back) C-f (forward) for snippet placeholder navigation.
+    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<CR>'] = cmp.mapping.confirm {
+      behavior = cmp.ConfirmBehavior.Replace,
+      select = true,
+    },
+    ['<Tab>'] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      else
+        fallback()
+      end
+    end, { 'i', 's' }),
+    ['<S-Tab>'] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      elseif luasnip.jumpable(-1) then
+        luasnip.jump(-1)
+      else
+        fallback()
+      end
+    end, { 'i', 's' }),
+  }),
+  sources = {
+    { name = 'nvim_lsp' },
+  },
+}
 
 -- Global mappings.
 -- See `:help vim.diagnostic.*` for documentation on any of the below functions
