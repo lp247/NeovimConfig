b278d0f4ac9be0703be5b4fd97c33348b870ee3f
Peter Lehnhardt <peter.lehnhardt@sap.com>
2024-05-16 09:56:08 +0000

    Clone instead of zip

--- install.sh
+++ install.sh
@@ -1,1 +1,1 @@
-curl -s -L https://github.com/lp247/NeovimConfig/archive/refs/heads/master.zip -o $HOME/file.zip && unzip -qq $HOME/file.zip -d $HOME && rm $HOME/file.zip && ln -s $HOME/NeovimConfig-master/nvim $HOME/.config/nvim && echo "Configuration installed successfully"
+git clone https://github.com/lp247/NeovimConfig.git $HOME/NeovimConfig && ln -s $HOME/NeovimConfig/nvim $HOME/.config/nvim && echo "Configuration installed successfully"
