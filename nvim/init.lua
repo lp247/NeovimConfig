299ff5fba9cf0bc905626bfe1a7d089be7a19180
Peter Lehnhardt <peter.lehnhardt@sap.com>
2024-05-31 07:55:15 +0000

    Install and setup telescope

--- nvim/init.lua
+++ nvim/init.lua
@@ -183,6 +183,40 @@ require("lazy").setup({
         end,
     },
 
+    -- Tooling
+    {
+        "nvim-telescope/telescope.nvim",
+        branch = "0.1.x",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            {
+                "nvim-telescope/telescope-live-grep-args.nvim" ,
+                version = "^1.0.0"
+            },
+        },
+        config = function()
+            local telescope = require('telescope')
+            local actions = require('telescope.actions')
+            telescope.setup {
+                defaults = {
+                    mappings = {
+                        i = {
+                            ['<C-l>'] = actions.cycle_history_next,
+                            ['<C-h>'] = actions.cycle_history_prev
+                        }
+                    }
+                }
+            }
+
+            local builtin = require('telescope.builtin')
+            vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+            vim.keymap.set("n", "<leader>fg", ":lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>")
+            vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+            vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+            telescope.load_extension("live_grep_args")
+        end
+    },
+
     -- Others
     "lambdalisue/suda.vim",
     "github/copilot.vim"
